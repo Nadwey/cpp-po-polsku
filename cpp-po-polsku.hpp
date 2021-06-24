c8b1785f93d1ad3b950f9ce472f3d61982061276
Nadwey <81181783+Nadwey@users.noreply.github.com>
2021-06-24 16:46:49 +0200

    Create cpp-po-polsku.hpp

---
+++ cpp-po-polsku.hpp
@@ -0,0 +1,67 @@
+#define StandardowaBiblioteka std
+
+#define Stała const
+#define Całkowita int
+#define Cośtam float
+#define Podwójna double
+#define Znak char
+#define Długa long
+// Lepszej nazwy nie było :))
+#define Zmienna1Bitowa bool
+// Z tłumacza
+#define Niepodpisana unsigned
+
+#define Publiczna public
+#define Prywatna private
+#define Chroniona protected
+#define Klasa class
+// Z tłumacza
+#define Wyliczenie enum
+#define Struktura struct
+#define DeklarujTyp typedef
+#define pusta void
+#define SzerokiZnak wchar_t
+
+#define RównaSię =
+#define Dodaj +
+#define Odejmij -
+#define Podziel /
+#define Pommnóż *
+#define DodajJeden ++
+#define OdejmijJeden --
+#define DodajTo +=
+#define OdejmijTo -=
+#define PomnóżTo *=
+#define PodzielTo /=
+#define Odnośnik &
+
+#define Nie !
+#define JestRówne ==
+#define NieJestRówne !=
+#define JestWiększe >
+#define JestMniejsze <
+#define JestWiększeLubRówne >=
+#define JestMniejszeLubRówne <=
+#define PrzesunięcieBitoweWLewo <<
+#define PrzesunięcieBitoweWPrawo >>
+#define I &&;
+#define Lub ||;
+
+#define Jeśli if
+#define Dla for
+#define Dopóki while
+#define Rób do
+#define Zwróć return
+#define Przerwij break
+#define Kontynuuj continue
+
+#include <iostream>
+#define StrumieńWyjściowy cout
+#define StrumieńWejściowy cin
+#define StrumieńBłędu cerr
+#define StrumieńLogu clog
+#define StrumieńWyjściowyW wcout
+#define StrumieńWejściowyW wcin
+#define StrumieńBłęduW wcerr
+#define StrumieńLoguW wclog
+#define ZakończLinie endl
